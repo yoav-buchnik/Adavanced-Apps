585812818aa0dfc87fa26b83ccd17a28f8aff2fd
unknown <yoavbu1003@gmail.com>
2025-01-17 17:00:09 +0200

    fix register

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -9,97 +9,99 @@ import { userInfo } from "os";
 
 
 const register = async (req: Request, res: Response, next): Promise<void> => {
- console.log('register')
- const email = req.body.email
- const password = req.body.password
+  const email = req.body.email
+  const username = req.body.username
+  const password = req.body.password
 
- if (!email || !password){
-  res.status(config.statusCode.BAD_REQUEST).json("Incomplete user data provided.");
- }
-
- try {
-  const user = await User.findOne({'email' : email})
-  if(user){
-    res.status(config.statusCode.BAD_REQUEST).json("user already exist.");
+  if (!email || !password || !username) {
+    res.status(config.statusCode.BAD_REQUEST).json("Incomplete user data provided.");
+    return;
   }
- } catch (error) {
-  res.status(config.statusCode.INTERNAL_SERVER_ERROR).json((error as Error).message);
- }
 
- //create the user
- try {
-  const salt = await bcrypt.genSalt(10)
-  const encryptedPassword = await bcrypt.hash(password, salt)
-  const user = new User({
-    'email' : email,
-    'password' : encryptedPassword,
-    'username' : req.body.username,
-  })
-  const newUser = await user.save()
-  res.status(config.statusCode.SUCCESS).json(newUser);
- } catch (error) {
-  res.status(config.statusCode.INTERNAL_SERVER_ERROR).json((error as Error).message);
- }
+  try {
+    const user = await User.findOne({ 'email': email })
+    if (user) {
+      res.status(config.statusCode.BAD_REQUEST).json("user already exist.");
+      return;
+    }
+  } catch (error) {
+    res.status(config.statusCode.INTERNAL_SERVER_ERROR).json((error as Error).message);
+    return;
+  }
 
+  //create the user
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const encryptedPassword = await bcrypt.hash(password, salt)
+    const user = new User({
+      'email': email,
+      'password': encryptedPassword,
+      'username': req.body.username,
+    })
+    const newUser = await user.save()
+    res.status(config.statusCode.SUCCESS).json(newUser);
+  } catch (error) {
+    res.status(config.statusCode.INTERNAL_SERVER_ERROR).json((error as Error).message);
+  }
 };
 
 const login = async (req: Request, res: Response, next): Promise<void> => {
- console.log('login')
- const email = req.body.email;
- const password = req.body.password;
- if (!email || !password){
-  res.status(config.statusCode.BAD_REQUEST).json("Incomplete user data provided.");
- }
- try {
-  const user = await User.findOne({'email':email});
-  if (user == null){
-    res.status(config.statusCode.BAD_REQUEST).json("Bad email or password.");
+  console.log('login')
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email || !password) {
+    res.status(config.statusCode.BAD_REQUEST).json("Incomplete user data provided.");
   }
-  const match = await bcrypt.compare(password, user.password);
-  if (!match){
-    res.status(config.statusCode.BAD_REQUEST).json("Bad email or password.");
+  try {
+    const user = await User.findOne({ 'email': email });
+    if (user == null) {
+      res.status(config.statusCode.BAD_REQUEST).json("Bad email or password.");
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      res.status(config.statusCode.BAD_REQUEST).json("Bad email or password.");
+    }
+    const accessToken = await jwt.sign(
+      { '_id': user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: process.env.JWT_TOKEN_EXPIRATION }
+    );
+    const refreshToken = await jwt.sign(
+      { '_id': user._id },
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    if (!user.tokens) {
+      user.tokens = [refreshToken]
+    }
+    else {
+      user.tokens.push(refreshToken)
+    }
+    await user.save()
+    res.status(config.statusCode.SUCCESS).json({ 'accessToken': accessToken, 'refreshToken': refreshToken });
+  } catch (error) {
+    res.status(config.statusCode.INTERNAL_SERVER_ERROR).json((error as Error).message);
   }
-  const accessToken = await jwt.sign(
-    {'_id': user._id},
-    process.env.ACCESS_TOKEN_SECRET,
-    {expiresIn: process.env.JWT_TOKEN_EXPIRATION}
-  );
-  const refreshToken = await jwt.sign(
-    {'_id': user._id},
-    process.env.REFRESH_TOKEN_SECRET
-  );
-  if (!user.tokens){
-    user.tokens = [refreshToken]
-  }
-  else{
-    user.tokens.push(refreshToken)
-  }
-  await user.save()
-  res.status(config.statusCode.SUCCESS).json({'accessToken':accessToken, 'refreshToken': refreshToken});
- } catch (error) {
-  res.status(config.statusCode.INTERNAL_SERVER_ERROR).json((error as Error).message);
- }
 };
 
 const logout = async (req: Request, res: Response, next): Promise<void> => {
   console.log('logout')
   const authHeaders = req.headers['authorization']
   const token = authHeaders && authHeaders.split(' ')[1]
-  if (!token){
+  if (!token) {
     res.status(config.statusCode.UNAUTHORIZED).json();
   }
 
   jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, async (err, userInfo) => {
-    if (err){
+    if (err) {
       res.status(config.statusCode.FORBIDDEN).json(err.message);
     }
     const userId = userInfo._id
     try {
       const user = await User.findById(userId)
-      if (!user){
+      if (!user) {
         res.status(config.statusCode.FORBIDDEN).json("invalid request");
       }
-      if (!user.tokens.includes(token)){
+      if (!user.tokens.includes(token)) {
         user.tokens = [""]
         await user.save()
         res.status(config.statusCode.FORBIDDEN).json("invalid request");
@@ -117,44 +119,44 @@ const refreshToken = async (req: Request, res: Response, next): Promise<void> =>
   console.log('refreshToken')
   const authHeaders = req.headers['authorization']
   const token = authHeaders && authHeaders.split(' ')[1]
-  if (!token){
+  if (!token) {
     res.status(config.statusCode.UNAUTHORIZED).json();
   }
 
   jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, async (err, userInfo) => {
-    if (err){
+    if (err) {
       res.status(config.statusCode.FORBIDDEN).json(err.message);
     }
     const userId = userInfo._id
     try {
       const user = await User.findById(userId)
-      if (!user){
+      if (!user) {
         res.status(config.statusCode.FORBIDDEN).json("invalid request");
       }
-      if (!user.tokens.includes(token)){
+      if (!user.tokens.includes(token)) {
         user.tokens = [""]
         await user.save()
         res.status(config.statusCode.FORBIDDEN).json("invalid request");
       }
 
       const accessToken = await jwt.sign(
-        { '_id':user._id },
+        { '_id': user._id },
         process.env.ACCESS_TOKEN_SECRET,
-        {expiresIn: process.env.JWT_TOKEN_EXPIRATION}
+        { expiresIn: process.env.JWT_TOKEN_EXPIRATION }
       )
       const refreshToken = await jwt.sign(
-        {'_id': user._id},
+        { '_id': user._id },
         process.env.REFRESH_TOKEN_SECRET
       )
       user.tokens[user.tokens.indexOf(token)] = refreshToken
       await user.save()
-      res.status(config.statusCode.SUCCESS).json({'accessToken':accessToken, 'refreshToken': refreshToken});
+      res.status(config.statusCode.SUCCESS).json({ 'accessToken': accessToken, 'refreshToken': refreshToken });
     } catch (error) {
       res.status(config.statusCode.FORBIDDEN).json((error as Error).message);
     }
   })
-  
- };
+
+};
 
 export default {
   register,
