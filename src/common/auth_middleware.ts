1516638262d30056d694d15016bed11e66b55c1d
unknown <yoavbu1003@gmail.com>
2025-01-17 17:49:31 +0200

    add error handler to auth

--- src/common/auth_middleware.ts
+++ src/common/auth_middleware.ts
@@ -3,19 +3,25 @@ import jwt from 'jsonwebtoken';
 import config from "../config/config";
 
 const authenticate = async (req: Request, res: Response, next) => {
-    const authHeaders = req.headers['authorization']
-    const token = authHeaders && authHeaders.split(' ')[1]
-    if (!token) {
-        res.status(config.statusCode.UNAUTHORIZED).json();
-    }
-    else {
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) {
-                res.status(config.statusCode.FORBIDDEN).json(err.message);
-            }
-            req.body.user = user
-            next()
-        })
+    try {
+        const authHeaders = req.headers['authorization'];
+        if (!authHeaders) {
+            throw new Error('No authorization header');
+        }
+
+        const token = authHeaders.split(' ')[1];
+        if (!token) {
+            throw new Error('No token provided');
+        }
+
+        const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        req.body.user = user;
+        next();
+    } catch (error) {
+        if (error.message === 'No authorization header' || error.message === 'No token provided') {
+            return res.status(config.statusCode.UNAUTHORIZED).json({ message: error.message });
+        }
+        return res.status(config.statusCode.FORBIDDEN).json({ message: error.message });
     }
 }
 
